a2402090f31038fbbfe8dad2f1bf176069a8eb15
varustomyan <v.arustomyan@icloud.com>
2022-06-25 15:05:01 +0300

    style: correction of typos

--- src/Page/HomePage/HomePage.jsx
+++ src/Page/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "../../Components/UI/Loader/Loader.jsx";
 import RestApi from "../../Components/API/RestApi.jsx";
 import { useFetching } from "../../Components/Hooks/useFetching";
@@ -7,24 +7,25 @@ import Filter from "../../Components/Filter/Filter.jsx";
 import CardList from "../../Components/CardList.jsx";
 
 const HomePage = () => {
-  const [arrConries, setArrContries] = useState([]);
+  const [arrCountries, setArrCountries] = useState([]);
   const [filter, setFilter] = useState({ query: "", select: "" });
 
-  const searchedCoutry = useCountry(arrConries, filter.select, filter.query);
+  const searchedCountry = useCountry(arrCountries, filter.select, filter.query);
 
   const [fetchCounties, isLoading] = useFetching(async () => {
-    const responce = await RestApi.getAll();
-    setArrContries(responce);
+    const response = await RestApi.getAll();
+    setArrCountries(response);
   });
 
   useEffect(() => {
     fetchCounties();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div>
       <Filter filter={filter} setFilter={setFilter} />
-      {isLoading ? <Loader /> : <CardList countries={searchedCoutry} />}
+      {isLoading ? <Loader /> : <CardList countries={searchedCountry} />}
     </div>
   );
 };
