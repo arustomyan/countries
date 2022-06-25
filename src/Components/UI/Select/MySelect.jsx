5caa994b5116612214985d8c6596b05d8b9c9db6
varustomyan <v.arustomyan@icloud.com>
2022-06-25 15:14:06 +0300

    style: add prettier

--- src/Components/UI/Select/MySelect.jsx
+++ src/Components/UI/Select/MySelect.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import whiteTheme from './SelectWhiteMode.module.css'
+import React from "react";
+import whiteTheme from "./SelectWhiteMode.module.css";
 
-const MySelect = ({option, defaultValue, value, onChange, style}) => {
-
-
-
-
-    return (
-            <select
-            value={value}
-            onChange={event => {
-                onChange(event.target.value)
-            }}
-            className={whiteTheme.selectRegion}
-            style={style}
+const MySelect = ({ option, defaultValue, value, onChange, style }) => {
+  return (
+    <select
+      value={value}
+      onChange={(event) => {
+        onChange(event.target.value);
+      }}
+      className={whiteTheme.selectRegion}
+      style={style}
+    >
+      <option disabled={true} value="">
+        {defaultValue}
+      </option>
+      {option.map((option) => (
+        <option
+          key={option.value}
+          value={option.value}
+          className={whiteTheme.option}
         >
-            <option disabled={true} value="">{defaultValue}</option>
-            {option.map(option =>
-                <option key={option.value} value={option.value} className={whiteTheme.option}>
-                    {option.name} 
-                </option>
-            )}
-        </select>
-        
-    );
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
 };
 
 export default MySelect;
-
