a74832e31371c838f0ed92411b2d0428283bc9e5
varustomyan <v.arustomyan@icloud.com>
2022-08-03 06:02:19 +0300

    style: fix eslint errors
    added ignore

--- src/Components/UI/Input/MyInput.jsx
+++ src/Components/UI/Input/MyInput.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import darkTheme from "./MyInputDarkMode.module.css";
 import whiteTheme from "./MyInputWhiteMode.module.css";
+// eslint-disable-next-line import/no-cycle
 import { DarkModeContext } from "../../../App";
 
 const MyInput = React.forwardRef((props, ref) => {
   const darkMode = useContext(DarkModeContext);
   const classes = darkMode ? whiteTheme : darkTheme;
 
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <input ref={ref} className={classes.search} {...props} />;
 });
 
