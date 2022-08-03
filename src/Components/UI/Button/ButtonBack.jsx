ee98ca64d458c366c288ca79eba20f7716f4e728
varustomyan <v.arustomyan@icloud.com>
2022-08-03 05:27:38 +0300

    refactor: fix eslint errors
    ignore everything, fix later together where used

--- src/Components/UI/Button/ButtonBack.jsx
+++ src/Components/UI/Button/ButtonBack.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import darkTheme from "./MyButtonDarkMode.module.css";
 import whiteTheme from "./MyButtonWhiteMode.module.css";
+// eslint-disable-next-line import/no-cycle
 import { DarkModeContext } from "../../../App";
 
+// eslint-disable-next-line react/prop-types
 function MyButton({ children, ...props }) {
   const darkMode = useContext(DarkModeContext);
   const classes = darkMode ? whiteTheme : darkTheme;
 
   return (
-    <button {...props} className={classes.btn}>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <button {...props} className={classes.btn} type="button">
       <svg focusable="false" width="51" height="13" viewBox="0 1 19 12">
         <path d="M6.46447 0.107445L7.64298 1.28596L3.75389 5.17504L18.6031 5.17504L18.6031 6.82496L3.75389 6.82496L7.64298 10.714L6.46447 11.8926L0.57191 6L6.46447 0.107445Z" />
       </svg>
