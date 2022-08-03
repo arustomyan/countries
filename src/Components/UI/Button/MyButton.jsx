b9685f8950d6e17386f5f5c09f5bdee6e7c9197c
varustomyan <v.arustomyan@icloud.com>
2022-08-03 05:45:51 +0300

    refactor: fix eslint errors
    ignore everything, fix later together where used, add atribute type in
    button

--- src/Components/UI/Button/MyButton.jsx
+++ src/Components/UI/Button/MyButton.jsx
@@ -1,14 +1,17 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import darkTheme from "./MyButtonDarkMode.module.css";
 import whiteTheme from "./MyButtonWhiteMode.module.css";
+// eslint-disable-next-line import/no-cycle
 import { DarkModeContext } from "../../../App";
 
+// eslint-disable-next-line react/prop-types
 function MyButton({ children, ...props }) {
   const darkMode = useContext(DarkModeContext);
   const classes = darkMode ? whiteTheme : darkTheme;
 
   return (
-    <button {...props} className={classes.btn}>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <button {...props} className={classes.btn} type="button">
       {children}
     </button>
   );
