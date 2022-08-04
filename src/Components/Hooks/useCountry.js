23c25d7b3d8b11a890707ce58c9597da03988979
varustomyan <v.arustomyan@icloud.com>
2022-08-04 13:24:20 +0300

    refactor: fix eslint errors
    added strict comparison, refactor

--- src/Components/Hooks/useCountry.js
+++ src/Components/Hooks/useCountry.js
@@ -1,25 +1,20 @@
 import { useMemo } from "react";
 
-export const useSelect = (countries, select) => {
-  const filteredCountries = useMemo(() => {
+export const useSelect = (countries, select) =>
+  useMemo(() => {
     if (!select) return countries;
 
-    return countries.filter((country) => {
-      return country.region == select;
-    });
+    return countries.filter((country) => country.region === select);
   }, [countries, select]);
-
-  return filteredCountries;
-};
 
 export const useCountry = (countries, select, query) => {
   const filteredCountries = useSelect(countries, select);
 
-  const searchedCountries = useMemo(() => {
-    return filteredCountries.filter((country) =>
-      country.name.common.toLowerCase().includes(query)
-    );
-  }, [query, countries, select]);
-
-  return searchedCountries;
+  return useMemo(
+    () =>
+      filteredCountries.filter((country) =>
+        country.name.common.toLowerCase().includes(query)
+      ),
+    [filteredCountries, query]
+  );
 };
