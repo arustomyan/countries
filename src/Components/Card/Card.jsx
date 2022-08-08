0d7adfb73dcffed89463c0f6e45b757809543628
varustomyan <v.arustomyan@icloud.com>
2022-08-08 16:18:57 +0300

    refactor: improving semantics
    Added a component <Link/>, now each card is a link

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,5 @@
+import { Link } from "react-router-dom";
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import classes from "./CardWhite.module.css";
 import DarkModeContext from "../../context/context";
 
@@ -9,41 +9,37 @@ function Card({ country }) {
     ? { backgroundColor: "#fff" }
     : { backgroundColor: "#2B3844" };
 
-  const router = useNavigate();
-
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions,jsx-a11y/no-noninteractive-element-interactions
-    <li
-      className={classes.card}
-      style={classes1}
-      onClick={() => router(`/${country.cca3}`)}
-    >
-      <div className={classes.imgWrapper}>
-        <img
-          className={classes.cardImage}
-          src={country.flags.png}
-          alt={`Flag of ${country.name.common}`}
-        />
-      </div>
-
-      <div className={classes.cardContentWrapper}>
-        <p className={classes.cardName}>{country.name.common}</p>
-        <div className={classes.cardInfoWrapper}>
-          <p className={classes.cardText}>
-            <span className={classes.cardSpan}>Population of: </span>
-            {country.population}
-          </p>
-          <p className={classes.cardText}>
-            <span className={classes.cardSpan}>Region: </span> {country.region}
-          </p>
-          <p className={classes.cardText}>
-            <span className={classes.cardSpan}>Capital: </span>{" "}
-            {!(country.capital === undefined)
-              ? country.capital[0]
-              : "no capital"}
-          </p>
+    <li className={classes.card} style={classes1}>
+      <Link to={`/${country.cca3}`}>
+        <div className={classes.imgWrapper}>
+          <img
+            className={classes.cardImage}
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+          />
         </div>
-      </div>
+
+        <div className={classes.cardContentWrapper}>
+          <p className={classes.cardName}>{country.name.common}</p>
+          <div className={classes.cardInfoWrapper}>
+            <p className={classes.cardText}>
+              <span className={classes.cardSpan}>Population of: </span>
+              {country.population}
+            </p>
+            <p className={classes.cardText}>
+              <span className={classes.cardSpan}>Region: </span>{" "}
+              {country.region}
+            </p>
+            <p className={classes.cardText}>
+              <span className={classes.cardSpan}>Capital: </span>{" "}
+              {!(country.capital === undefined)
+                ? country.capital[0]
+                : "no capital"}
+            </p>
+          </div>
+        </div>
+      </Link>
     </li>
   );
 }
